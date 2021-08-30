c057f6216da935e4ac6565e0aca500c25f7adf40
daileytj <daileytj89@gmail.com>
2021-08-30 15:22:08 -0400

    Add blueDarkAlt theme deprecation notice for v6.0.0

--- src/blueDarkAltTheme.ts
+++ src/blueDarkAltTheme.ts
@@ -9,6 +9,9 @@ This code is licensed under the BSD-3 license found in the LICENSE file in the r
 import { blue, black, lightBlue } from '@pxblue/colors';
 import { blueDarkTheme } from './blueDarkTheme';
 
+  /**
+     * @deprecated in version 6.0.0
+     */
 export const blueDarkAltTheme = {
     ...blueDarkTheme,
     colors: {
